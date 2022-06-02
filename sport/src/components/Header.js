0f6ada434044b7c5b6d4e55dcf90ce9f0d9fc2c2
Nicolas <nicolasmilloco@hotmail.com>
2022-06-02 18:06:37 +0200

    création du composant header

---
+++ sport/src/components/Header.js
@@ -0,0 +1,23 @@
+import logo from '../assets/logo.svg';
+
+
+/**
+ * Création de la barre de navigation
+ * @returns {JSX}
+ */
+
+const Header = () => {
+  return (
+    <header className="header">
+        <img src={logo} className="header_logo" alt="" />
+        <ul className="header_list">
+            <li className="header_list_name">Accueil</li>
+            <li className="header_list_name">Profil</li>
+            <li className="header_list_name">Réglages</li>
+            <li className="header_list_name">Communauté</li>
+        </ul>
+    </header>
+  );
+};
+
+export default Header;
