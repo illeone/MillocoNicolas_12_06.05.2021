58c92ebb4dfae863540bccc2dac456a4f7dc44fa
Nicolas <nicolasmilloco@hotmail.com>
2022-06-05 19:04:26 +0200

    css logos barre latérale

--- sport/src/App.js
+++ sport/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import LateralBar from './components/LateralBar';
 
 function App() {
   return (
 
         <>
         <Header />
+        <LateralBar />
         </>
         
       );
--- sport/src/components/LateralBar.js
+++ sport/src/components/LateralBar.js
@@ -2,3 +2,32 @@ import yoga from '../assets/yoga.svg';
 import swim from '../assets/swim.svg';
 import bike from '../assets/bike.svg';
 import alters from '../assets/alters.svg';
+
+const LateralBar = () => {
+    return (
+      <>
+        <section className="header lateral">
+          <nav className="header_vertical">
+            <ul className="header_icons">
+              <li className="lateral_icon">
+                <img className="lateral_img" src={yoga} alt="" />
+              </li>
+              <li className="lateral_icon">
+                <img className="lateral_img" src={swim} alt="" />
+              </li>
+              <li className="lateral_icon">
+                <img className="lateral_img" src={bike} alt="" />
+              </li>
+              <li className="lateral_icon">
+                <img className="lateral_img"  src={alters} alt="" />
+              </li>
+            </ul>
+            <p className="lateral_copyrights">Copiryght, SportSee 2020</p>
+          </nav>
+        </section>
+      </>
+    );
+};
+
+export default LateralBar;
+
