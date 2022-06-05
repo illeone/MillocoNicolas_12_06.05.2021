d6558167b325368b65d9b53d449fa8ddaf41fb3d
Nicolas <nicolasmilloco@hotmail.com>
2022-06-05 21:08:04 +0200

    création page dashboard

--- sport/src/App.js
+++ sport/src/App.js
@@ -1,12 +1,14 @@
-import Header from './components/Header';
-import LateralBar from './components/LateralBar';
+// import Header from './components/Header';
+// import LateralBar from './components/LateralBar';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
 
         <>
-        <Header />
-        <LateralBar />
+        {/* <Header />
+        <LateralBar /> */}
+        <Dashboard />
         </>
         
       );
---
+++ sport/src/pages/Dashboard.js
@@ -0,0 +1,24 @@
+import Header from "../components/Header";
+import LateralBar from "../components/LateralBar";
+
+
+const Dashboard = () => {
+
+    return (
+        <>
+
+        <Header />
+        <LateralBar />
+
+        <div class="">
+            <div class="">
+                <span className="">Bonjour <span className="">Thomas</span></span>
+                <p className="">Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+            </div>
+        </div>
+
+        </>
+    )
+}
+
+export default Dashboard;
