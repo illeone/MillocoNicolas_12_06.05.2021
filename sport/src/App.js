b4bde4eb5938dcf0eb32d26dff8d1aea9d6615ac
Nicolas <nicolasmilloco@hotmail.com>
2022-06-09 16:07:08 +0200

    création page userspage et mise en place dans app.js

--- sport/src/App.js
+++ sport/src/App.js
@@ -1,15 +1,25 @@
 // import Header from './components/Header';
 // import LateralBar from './components/LateralBar';
 import Dashboard from './pages/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import LateralBar from './components/LateralBar';
+import UsersPage from './pages/UsersPage';
 
 function App() {
   return (
-
-        <>
-        {/* <Header />
-        <LateralBar /> */}
-        <Dashboard />
-        </>
+        <div>
+          <Router>
+            <Header />
+            <main>
+              <LateralBar />
+                <Routes>
+                  <Route path="/" element={<UsersPage/>} />
+                  <Route path="/user/:id" element={<Dashboard />} />  
+                </Routes> 
+            </main>  
+          </Router>
+        </div>
         
       );
 }
---
+++ sport/src/pages/UsersPage.js
@@ -0,0 +1,16 @@
+const UsersPage = () => {
+
+    return (
+        <>
+        <div className="">
+            <div className="">
+                <button className="">12</button>
+                <button className="">18</button>
+            </div>
+        </div>
+
+        </>
+    )
+}
+
+export default UsersPage;
