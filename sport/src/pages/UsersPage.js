589bba895e7c908c8fbffa5e93a7bc673c107bcd
Nicolas <nicolasmilloco@hotmail.com>
2022-06-15 17:56:46 +0200

    modification liens navLink 12/18

--- sport/src/pages/UsersPage.js
+++ sport/src/pages/UsersPage.js
@@ -6,11 +6,11 @@ const UsersPage = () => {
         <>
         <div className="">
             <nav className="aze">
-                <NavLink to="/user/:id" className="link" style={{ textDecoration: 'none' }}>
+                <NavLink to="/user/12" className="link" style={{ textDecoration: 'none' }}>
                     <div className="btn">12</div> 
                 </NavLink>
                 
-                <NavLink to="/user/:id" className="link" style={{ textDecoration: 'none' }}>
+                <NavLink to="/user/18" className="link" style={{ textDecoration: 'none' }}>
                     <div className="btn btn2">18</div>
                 </NavLink>
             </nav>
