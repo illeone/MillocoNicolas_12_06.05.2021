e821471e0af90ce49a91edb8a22cebe5782c9cdb
Nicolas <nicolasmilloco@hotmail.com>
2022-06-20 12:49:53 +0200

    création composant Score

---
+++ sport/src/components/Score.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { PieChart, Pie, Cell } from 'recharts';
+
+
+function Score({data}) {
+    
+    const data01 = [
+        { value: data.todayScore },
+        { value: 1 - data.todayScore }, 
+    ];
+    
+    return (
+        <div className="score">        
+            <h1 className='score_title'>Score</h1>
+            <div className='score_circle'>
+                <PieChart width={258} height={263}>       
+                    <Pie 
+                    data={data01}
+                    dataKey="value"
+                    innerRadius={70}
+                    outerRadius={80}
+                    startAngle={90}
+                    endAngle={450}
+                    stroke="transparent"
+                    >
+
+                        {data01.map((entry, index) => (
+                            <Cell
+                                key={index}
+                                fill={index === 0 ? '#FF0000' : '#fbfbfb'}
+                                cornerRadius="50%"
+                            />
+                        ))}
+                    </Pie>
+
+                 <circle cx="130" cy="133" r="70" fill="#ffffff" />                     
+                </PieChart>  
+
+                <div className="score_infos">
+                    <p className="score_pourcentage" >
+                        {data01[0].value * 100 }% 
+                    </p>
+                    <p className="score_text">de votre objectif</p>
+                </div>  
+            </div>   
+        </div>
+      );
+}
+
+export default Score;
