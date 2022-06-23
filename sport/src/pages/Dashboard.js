676bcdd6669b81e7322a10da1a12670c773b003d
Nicolas <nicolasmilloco@hotmail.com>
2022-06-23 10:51:04 +0200

    Ajout composant Average dans Dashboard

--- sport/src/pages/Dashboard.js
+++ sport/src/pages/Dashboard.js
@@ -5,66 +5,51 @@ import Users from '../components/Users';
 import Score from '../components/Score';
 import InfoList from '../components/InfoList';
 import Activity from '../components/Activity';
+import Average from '../components/Average.js';
 
 import calorie from '../assets/calorie.svg'
 import protein from '../assets/protein.svg'
 import carb from '../assets/carb.svg'
 import lipid from '../assets/lipid.svg'
-// import { useEffect, useState } from 'react';
-// import axios from 'axios';
-
 
 
 function Dashboard() {
     const {id} = useParams();
     const userInfos = useApi(`../${id}/users.json`);
     const activity = useApi(`../${id}/activity.json`);
+    const average = useApi(`../${id}/average.json`);
 
     if (userInfos === null || activity === null) {
         console.log("chargement");
-        return <p> chargement</p>      
+        return <p>chargement...</p>      
     }
 
-    // if (userInfos.length === 0) return null;
-
-
-    console.log(userInfos.data.firstName);
-    console.log(activity);
-
-    // const [data, setData] = useState([]);
-        
-    // useEffect((url) => {
-
-    //             const res =  axios.get(url);
-    //             setData(res.data);
-	// 			console.log(res);
-
-    //     }, []);
-
-        // if (userInfos.length === 0) return null;
-
-        // console.log(userInfos)
+    // console.log(userInfos.data.userInfos.firstName);
+    // console.log(activity);
 
     return (
         <>
         
-        <div className="">
-            
-            <div className="">           
-                {userInfos && <Users className="" name={userInfos.data.userInfos.firstName}/>}
-                {userInfos && <Score data={userInfos.data} />}
+            <div className="">
+                
+                <div className="">           
+                    {userInfos && <Users className="" name={userInfos.data.userInfos.firstName}/>}
+                    {userInfos && <Score today={userInfos.data.todayScore} />}
+                </div>
+
+                <aside>
+                    {userInfos && <InfoList icon={calorie} info={userInfos.data.keyData.calorieCount} value="kCal" title="Calories" />}
+                    {userInfos && <InfoList icon={protein} info={userInfos.data.keyData.proteinCount} value="g" title="Proteines" />}
+                    {userInfos && <InfoList icon={carb} info={userInfos.data.keyData.carbohydrateCount} value="g" title="Glucides" />}
+                    {userInfos && <InfoList icon={lipid} info={userInfos.data.keyData.lipidCount} value="g" title="Lipides" />} 
+                </aside>
+                {activity && <Activity data={activity.data.sessions} />}
+                <div>
+                    {average && <Average data={average.data.sessions} />}
+                </div>
+                
+
             </div>
-
-            <aside>
-                {userInfos && <InfoList icon={calorie} info={userInfos.data.keyData.calorieCount} value="kCal" title="Calories" />}
-                {userInfos && <InfoList icon={protein} info={userInfos.data.keyData.proteinCount} value="g" title="Proteines" />}
-                {userInfos && <InfoList icon={carb} info={userInfos.data.keyData.carbohydrateCount} value="g" title="Glucides" />}
-                {userInfos && <InfoList icon={lipid} info={userInfos.data.keyData.lipidCount} value="g" title="Lipides" />} 
-            </aside>
-            {userInfos && <Activity data={activity.data.sessions} />}
-            
-
-        </div>
 
         </>
     )
