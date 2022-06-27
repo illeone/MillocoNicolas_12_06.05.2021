b6179df65a0ff2806ab5be90f47ac509a2ced932
Nicolas <nicolasmilloco@hotmail.com>
2022-06-27 10:38:15 +0200

    passage à react 17

--- sport/src/components/Average.js
+++ sport/src/components/Average.js
@@ -2,16 +2,28 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts'
 
 function Average ({data}) {
 
-    function daysWeek(value) {
-        if (value === 1) return 'L'
-        if (value === 2) return 'M'
-        if (value === 3) return 'M'
-        if (value === 4) return 'J'
-        if (value === 5) return 'V'
-        if (value === 6) return 'S'
-        if (value === 7) return 'D'
-        return value
+    function weekDays(day) {
+        if (day === 1) return 'L'
+        if (day === 2) return 'M'
+        if (day === 3) return 'M'
+        if (day === 4) return 'J'
+        if (day === 5) return 'V'
+        if (day === 6) return 'S'
+        if (day === 7) return 'D'
+        return day
       }
+
+    function CustomToolTip({ active, payload }) {
+        if (active) {
+          return (
+            <div className='tool_tip_average'>         
+              <p className='tool_tip_average_min'>{payload[0].value} min</p>
+            </div>
+          )
+        } else {       
+        return null
+        }
+    }
 
     return (
 
@@ -32,6 +44,7 @@ function Average ({data}) {
             vertical={false}
             horizontal={false}
             />
+
             <XAxis
             dataKey="day"
             dy={-10}
@@ -39,21 +52,18 @@ function Average ({data}) {
             tickLine={false}
             mirror={true}
             tick={{fill:'#f9b7b7'}}
-            tickFormatter={daysWeek}
-
-
+            tickFormatter={weekDays}
             />
+
             <YAxis
             width={5}
             tickCount={false}
             domain={["dataMin-28", 'dataMax + 50']}
-            // domain={[4, "dataMax + 60"]}
-            
-            
             />
+
             <Tooltip 
-                
-                cursor={{ stroke: 'rgba(0, 0, 0, 0.1)', strokeWidth: 40, }}
+            content={<CustomToolTip active={true} payload={data} />}
+            cursor={{ stroke: 'rgba(0, 0, 0, 0.1)', strokeWidth: 40, }}
             />
 
             <Line
@@ -63,7 +73,6 @@ function Average ({data}) {
             stroke="#f9b7b7"         
             dot={false}
             activeDot={{ stroke: '#FFFFFF33', strokeWidth: 10, r: 5 }}
-            //   activeDot={<CustomActiveDot />}
             />
         </LineChart>
     </div>
--- sport/src/components/Performance.js
+++ sport/src/components/Performance.js
@@ -1,4 +1,4 @@
-import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis  } from 'recharts';
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
 
 function Performance ({data}) {
     console.log(data)
@@ -9,12 +9,14 @@ function Performance ({data}) {
     return(
         <>
             <div className='performance'>
-                <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
-                    <PolarGrid />
-                    <PolarAngleAxis dataKey="kind" tickLine={true} axisLine={true} tick={{ fontSize: 10 }} />
-                    <PolarRadiusAxis />
-                    <Radar dataKey="value" stroke="#FF0100" fill="#FF0100" fillOpacity={0.6} />
-                </RadarChart>   
+                <ResponsiveContainer width="100%" height="100%">
+                    <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
+                        <PolarGrid />
+                        <PolarAngleAxis dataKey="kind" tickLine={true} axisLine={true} tick={{ fontSize: 10 }} />
+                        <PolarRadiusAxis />
+                        <Radar dataKey="value" stroke="#FF0100" fill="#FF0100" fillOpacity={0.6} />
+                    </RadarChart> 
+                </ResponsiveContainer>
             </div>
             <div className="">{data.kind[1]} </div>
         </>
